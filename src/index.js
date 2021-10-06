046f09cd9c0b9bb0f885a99ce6a9e40cbb715ecd
Lucas Roman <lucasroman@gmail.com>
2021-10-06 10:00:37 -0300

    Remove default comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+<<<<<<< HEAD
 import App from './App';
 import Hello from './components/Hello';
+=======
+import App from './components/App';
+>>>>>>> d4ff115... Remove default comments
 import reportWebVitals from './reportWebVitals';
 
 // ReactDOM.render(
@@ -19,7 +23,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
