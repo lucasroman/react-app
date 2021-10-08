0c016f8f1c22d7a4a7b795ab9e728bb8e2a8691f
Lucas Roman <lucasroman@gmail.com>
2021-10-08 11:16:54 -0300

    Add a title to HookEffect component
    Also add to this one a new center style (title class).

--- src/components/HookEffect.js
+++ src/components/HookEffect.js
@@ -9,6 +9,7 @@ function Example() {
 
   return (
     <div>
+      <h1 className="title">HookEffect Component</h1>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
