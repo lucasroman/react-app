f2a53440172a9d8ae2871d3aa390d726319208d8
Lucas Roman <lucasroman@gmail.com>
2021-10-21 15:40:22 -0300

    Update hook context
    Now this component use hook context.

--- src/components/HookContext.js
+++ src/components/HookContext.js
@@ -1,28 +1,31 @@
-import { useState } from "react";
-import ReactDOM from "react-dom";
+import { useState, createContext, useContext } from "react";
+
+const UserContext = createContext();
 
 export default function Component1() {
-  const [user, setUser] = useState("Jesse Hall");
+  const [user] = useState("Jesse Hall");
 
   return (
-    <>
-      <h1>{`Hello ${user}`}</h1>
-      <Component2 user={user} />
-    </>
+    <UserContext.Provider value={user}>
+      <h1>{`Hello from first component, user: ${user}`}</h1>
+      <Component2 />
+    </UserContext.Provider>
   );
 }
 
-function Component2({ user }) {
+function Component2() {
   return(
     <>
       <h1>Component 2</h1>
-      <Component3 user={user} />
+      <Component3 />
 
     </>
   );
 }
 
-function Component3({ user }) {
+function Component3() {
+  const user = useContext(UserContext);
+
   return(
     <>
       <h1>Component 3</h1>
